8468edcd35005db3af985d2e0055a95d5e638e95
Xing Niu <xniu7@hawk.iit.edu>
2021-05-02 19:12:51 -0500

    rm m

--- monetdb/tpch/nor_sql/q2u.sql
+++
@@ -1,6 +0,0 @@
-select s_acctbal,s_name,n_name,p_partkey,p_mfgr,s_address,s_phone,s_comment
-from part, supplier,partsupp ps,nation,region, (select min(ps_supplycost) as min_supplycost, ps_partkey from partsupp, supplier, nation, region where s_suppkey = ps_suppkey and s_nationkey = n_nationkey and n_regionkey = r_regionkey and r_name = 'EUROPE' group by ps_partkey) m
-where p_partkey = ps.ps_partkey and s_suppkey = ps.ps_suppkey and p_size = 15 and p_type like '%BRASS' and s_nationkey = n_nationkey and n_regionkey = r_regionkey and r_name = 'EUROPE'
-and ps_supplycost = m.min_supplycost and p_partkey = m.ps_partkey
-order by s_acctbal desc, n_name, s_name, p_partkey
-LIMIT 100;
--- monetdb/tpch/sql/q15_10k_10g.sql
+++
@@ -1,6 +0,0 @@
-select s_suppkey, s_name, s_address, s_phone, total_revenue from (select * from supplier where ("s_suppkey" >= 10900) AND ("s_suppkey" < 10910)
-) ss1, (select supplier_no, total_revenue from (select l_suppkey as supplier_no, sum(l_extendedprice * (1 - l_discount)) as total_revenue from (select * from lineitem where 
-("l_suppkey" >= 10897) AND ("l_suppkey" < 10907)
-) ll1 where l_shipdate >= '1996-12-01' and l_shipdate < '1997-03-01' group by l_suppkey) l1, (select max(total_revenue) as total_revenue2 from (select l_suppkey as supplier_no, sum(l_extendedprice * (1 - l_discount)) as total_revenue from (select * from lineitem where 
-("l_suppkey" >= 10897) AND ("l_suppkey" < 10907)
-) ll2 where l_shipdate >= '1996-12-01' and l_shipdate < '1997-03-01' group by l_suppkey) l2) l3 where l1.total_revenue = l3.total_revenue2) l5 where s_suppkey = supplier_no order by s_suppkey;
